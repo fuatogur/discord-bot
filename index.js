d7d057abe5c5d01d1320d5c52aa3969699dcfe3d
fuatogur <58439290+fuatogur@users.noreply.github.com>
2020-04-10 10:18:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -97,10 +97,19 @@ bot.on('message',  message => {
                 .setTitle("Oylama Menüsü");
 
                 if(!arg[1]){
-                    message.channel.send(PollEmbed)
+                    message.channel.send(PollEmbed);
+                    break;
                 }
-                let msgArgs = arg.slice(1).join(" ")
-                message.channel.send(msgArgs)
+                 let msgArgs = arg.slice(1).join(" ");
+                const EmbedPoll = new Discord.MessageEmbed()
+                .setColor(0xffff00)
+                .setTitle("Oylama Menüsü")
+                .addField(msgArgs);
+               
+                message.channel.send(EmbedPoll).then(messageReaction => {
+                    messageReaction.react("👍");
+                    messageReaction.react("👎")
+                })
                 
             break;
         case "ban":
