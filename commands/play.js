da49c49d0d2c3699cd1b89451fdc8eb5124f1a51
fuatogur <58439290+fuatogur@users.noreply.github.com>
2020-04-12 17:09:29 +0300

    must be at a voice channel

--- commands/play.js
+++ commands/play.js
@@ -2,7 +2,9 @@ module.exports = {
     name: 'play',
     description: 'play a songr',
     run: async (message,arg) => {
-        if (!arg[1]) return message.reply("Lütfen link veya anahtar sözcük/sözcükler girin.")
+        if (!arg[1]) return message.reply("Lütfen link veya anahtar sözcük/sözcükler girin.");
+
+        if(!message.member.voiceChannel) return message.channel.send("Bir sesli sohbet kanalında bulunmanız gerekir.")
         
     }
 }
