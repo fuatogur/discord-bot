b90819e2520fec9881b3fa7a42e4cb4b6a1edbc7
fuatogur <58439290+fuatogur@users.noreply.github.com>
2020-05-10 20:33:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ commandFiles.forEach(file => {
     bot.commands.set(command.name, command)
 });
 
+// Initialize the invite cache
+const invites = {};
+
+// A pretty useful method to create a delay without blocking the whole script.
+const wait = require('util').promisify(setTimeout);
+
+bot.on('ready', () => {
+  // "ready" isn't really ready. We need to wait a spell.
+  wait(1000);
+
+  // Load all invites for all guilds and save them to the cache.
+  bot.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
 
 bot.on('message', message => {
 
@@ -144,7 +161,21 @@ bot.on('guildMemberAdd', async member => {
     ctx.drawImage(avatar, 25, 25, 200, 200);
 
     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-    channel.send(` 📢 Kullanıcı Katıldı! **1000** Kişi Olmamıza \`${1000-memberCounter()}\` Kişi Kaldı \`${memberCounter()}\` Kişiyiz! ✔️ ${member}`);
+    member.guild.fetchInvites().then(guildInvites => {
+        // This is the *existing* invites for the guild.
+        const ei = invites[member.guild.id];
+        // Update the cached invites for the guild.
+        invites[member.guild.id] = guildInvites;
+        // Look through the invites, find the one for which the uses went up.
+        const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+        // This is just to simplify the message being sent below (inviter doesn't have a tag property)
+        const inviter = client.users.get(invite.inviter.id);
+        // Get the log channel (change to your liking)
+        const logChannel = member.guild.channels.find(channel => channel.name === "join-logs");
+        // A real basic message with the information we need. 
+        logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
+      });
+    channel.send(` ${member}, ${inviter.tag} tarafından sunucuya katıldı. 📢 **1000** Kişi Olmamıza \`${1000-memberCounter()}\` Kişi Kaldı \`${memberCounter()}\` Kişiyiz! ✔️ `);
     
     channel.send(attachment);
     
