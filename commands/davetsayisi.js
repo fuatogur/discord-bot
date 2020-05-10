3f046427266bf6d5e003b39d0cb9d59ee2b20b29
fuatogur <58439290+fuatogur@users.noreply.github.com>
2020-05-10 23:27:52 +0300

    aaa

--- commands/davetsayisi.js
+++ commands/davetsayisi.js
@@ -22,28 +22,6 @@ bot.on('ready', () => {
     });
   });
 });
-        message.guild.fetchInvites().then(guildInvites => {
-            
-                // This is the *existing* invites for the guild.
-                var ei = invites[member.guild.id];
-                // Update the cached invites for the guild.
-                invites[member.guild.id] = guildInvites;
-                // Look through the invites, find the one for which the uses went up.
-                var invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-                // This is just to simplify the message being sent below (inviter doesn't have a tag property)
-                var inviter = bot.users.cache.get(invite.inviter.id);
-                // Get the log channel (change to your liking)
-                
-                //logChannel.send(` ${member}, ${inviter} tarafından sunucuya katıldı. 📢 **1000** Kişi Olmamıza \`${1000-memberCounter()}\` Kişi Kaldı \`${memberCounter()}\` Kişiyiz!. ${invite.uses} davet `);
-                // A real basic message with the information we need. 
-                //logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
-                
-                const embed = new Discord.MessageEmbed().setTitle("Kullanıcı Bilgisi")
-                .setColor("RANDOM")
-                .addField("Kullanıcı Adı", message.author.username, true)
-                .addField("Davet Edilen Kişi Sayısı", invite.uses, true)
-                .setThumbnail(message.author.displayAvatarURL())
-                .setTimestamp();
-              }).catch();
+        
             }
     }
